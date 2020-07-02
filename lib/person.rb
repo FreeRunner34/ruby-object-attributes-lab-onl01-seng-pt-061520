a13018e7b088da35fec30fecb1e81ab7301a6596
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-07-02 03:28:42 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person 
+  def name (this_dogs_name)
+    @name= this_dogs_namename 
+  end 
+  
+  def name 
+    @name
+  end 
+  
+  def breed (this_dogs_breed)
+    @breed = this_dogs_breed
+  end 
+  
+  def breed 
+    @breed 
+  end 
+  
+end 
