aa1fe9975cb016d6b627e85d35378e8d089ab56a
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-07-02 03:38:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,20 @@
-class Dog 
-  def name (this_dogs_name)
-    @name= this_dogs_name 
-  end 
+class Dog
   
-  def name 
-    @name
-  end 
+  def name=(dogs_name)
+    @name = dogs_name
+  end
   
-  def breed (this_dogs_breed)
-    @breed = this_dogs_breed
-  end 
+  def name
+    @name 
+  end
+  def breed=(breed)
+    @breed = breed
+  end
   
-  def breed 
-    @breed 
+  def breed
+    @breed
   end 
-  
-end 
+end
 
 fido = Dog.new
 
